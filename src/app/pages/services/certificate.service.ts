34d47932a70d4e25c9391fcdf7b0680d31b2718f
Adam Eggleston <adam.eggleston@auspost.com.au>
2019-01-24 16:32:43 +1100

    added the s to https

--- src/app/pages/services/certificate.service.ts
+++ src/app/pages/services/certificate.service.ts
@@ -28,7 +28,7 @@ export class CertificateService {
       })
     };
 
-    this.http.get(`http://s3-ap-southeast-2.amazonaws.com/ngau-hosting-certificates/${uniqueId}.pdf`, {responseType: ResponseContentType.Blob})
+    this.http.get(`https://s3-ap-southeast-2.amazonaws.com/ngau-hosting-certificates/${uniqueId}.pdf`, {responseType: ResponseContentType.Blob})
       .subscribe((response) => {
         const blob = new Blob([response.blob()], {type: 'application/pdf'});
         const filename = 'file.pdf';
