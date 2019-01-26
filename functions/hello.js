8ed15050b0a65afb0c8403e23742c42d9ba98761
Adam Eggleston <adam.eggleston@auspost.com.au>
2019-01-27 09:39:11 +1100

    bash script

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,7 @@
 exports.handler = async (event, context) => {
   const name = event.queryStringParameters.name || "World";
-
+  const execFile = require('child_process').execFile;
+  execFile('./test.sh'); 
   return {
     statusCode: 200,
     body: `Hello, ${name}`
