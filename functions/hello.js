2c15e6b9cb425513298d205cfe4ee946f177b98e
Adam Eggleston <adam.eggleston@auspost.com.au>
2019-01-27 11:15:09 +1100

    netlify automation

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,6 @@
 exports.handler = async (event, context) => {
   const name = event.queryStringParameters.name || "World";
-  const execFile = require('child_process').execFile;
-  execFile('./test.sh'); 
+
   return {
     statusCode: 200,
     body: `Hello, ${name}`
